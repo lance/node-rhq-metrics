768a4ea2489e86c0987aa123bcc66a53a3c8c6e6
Lance Ball <lball@redhat.com>
2014-12-15 21:16:10 -0500

    Remove IIFE. Not necessary

--- index.js
+++ index.js
@@ -1,66 +1,64 @@
 
-(function() {
-  require('date-utils');
-  var http = require('http');
-  var streamBuffers = require('stream-buffers');
+require('date-utils');
+var http = require('http');
+var streamBuffers = require('stream-buffers');
 
-  function RHQ(options) {
-    if (!this instanceof RHQ) return new RHQ(options);
-    options = options || {};
-    this.host = options.host || 'localhost';
-    this.port = options.port || '8080';
-    this.path = options.path || '/rhq-metrics/metrics';
+function RHQ(options) {
+  if (!this instanceof RHQ) return new RHQ(options);
+  options = options || {};
+  this.host = options.host || 'localhost';
+  this.port = options.port || '8080';
+  this.path = options.path || '/rhq-metrics/metrics';
+}
+
+RHQ.prototype.get = function(id, options, callback) {
+  if (!isString(id)) throw new TypeError('id must be a string');
+
+  if (!options) {
+    options = {};
+    callback = function(b) { console.log(b); };
+  } else if (typeof options === 'function') {
+    callback = options;
+    options = {};
   }
 
-  RHQ.prototype.get = function(id, options, callback) {
-    if (!isString(id)) throw new TypeError('id must be a string');
+  var path = [this.path, id].join('/') +
+             '?start=' + (options.start || new Date().removeHours(8).getTime()) +
+             '&end=' + (options.end || Date.now());
 
-    if (!options) {
-      options = {};
-      callback = function(b) { console.log(b); };
-    } else if (typeof options === 'function') {
-      callback = options;
-      options = {};
-    }
+  if (options.buckets) path += '&buckets=' + options.buckets;
 
-    var path = [this.path, id].join('/') +
-               '?start=' + (options.start || new Date().removeHours(8).getTime()) +
-               '&end=' + (options.end || Date.now());
-
-    if (options.buckets) path += '&buckets=' + options.buckets;
-
-    var httpOpts = {
-      hostname: this.host,
-      port: this.port,
-      path: path,
-    };
-
-    var request = http.get(httpOpts, function(res) {
-      var buffer = new streamBuffers.WritableStreamBuffer();
-
-      res.on('data', function(chunk) {
-        buffer.write(chunk);
-      });
-
-      res.on('end', function() {
-        process.nextTick(function() { 
-          callback(buffer.getContentsAsString('utf8')); 
-        });
-      });
-    });
-
-    request.on('error', function(e) {
-      // TODO: handle error and bubble to client
-      console.error('ERROR: ' + e);
-    });
-        
+  var httpOpts = {
+    hostname: this.host,
+    port: this.port,
+    path: path,
   };
 
+  var request = http.get(httpOpts, function(res) {
+    var buffer = new streamBuffers.WritableStreamBuffer();
 
-  function isString(s) {
-    return s && (typeof s === 'string');
-  }
+    res.on('data', function(chunk) {
+      buffer.write(chunk);
+    });
 
-  module.exports = RHQ;
-})();
+    res.on('end', function() {
+      process.nextTick(function() { 
+        callback(buffer.getContentsAsString('utf8')); 
+      });
+    });
+  });
+
+  request.on('error', function(e) {
+    // TODO: handle error and bubble to client
+    console.error('ERROR: ' + e);
+  });
+      
+};
+
+
+function isString(s) {
+  return s && (typeof s === 'string');
+}
+
+module.exports = RHQ;
 
