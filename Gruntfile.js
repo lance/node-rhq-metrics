7ec965b9e7e0084c65b565393088c549a71f9e86
Lance Ball <lball@redhat.com>
2014-12-16 12:46:52 -0500

    Change doc output name in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
       debug: {
         src: ['src/**/*.js'],
         options: {
-          output: 'docs/'
+          output: 'doc/'
         }
       }
     },
